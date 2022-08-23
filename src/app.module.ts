7fd1952a1f29b2cbac6ad569e0b468dad419a942
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2022-08-23 13:19:47 -0700

    made the necessary resource folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { GroupsModule } from './groups/groups.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, GroupsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

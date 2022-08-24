98b89ec29653a6cd66b563156080c9616d6162d2
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2022-08-23 20:20:19 -0700

    trying to get gcloud-storage file upload to work

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({
-      ignoreTrailingSlash: true,
-    }),
-  );
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
